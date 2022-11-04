2397ac77659cb771fadc61d4f08f3a7f16b8228f
Justin Liu <justinwl2097@gmail.com>
2022-11-04 12:45:09 -0500

    finished thought schema

---
+++ models/Thought.js
@@ -0,0 +1,44 @@
+const { Schema, Mongoose  } = require('mongoose');
+
+const reactionSchema = new Schema(
+    {
+        reactionId: {
+            type: new Mongoose.Types.ObjectId(),
+            default: new ObjectId()
+        },
+        reactionBody: {
+            type: String,
+            required: true,
+            maxLength: 280,
+        },
+        username: {
+            type: String,
+            required: true,
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now
+        }
+    }
+)
+
+const thoughtSchema = new Schema(
+    {
+        thoughtText: {
+            type: String,
+            required: true,
+            minLength: 1,
+            maxLength: 280
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+
+        },
+        username: {
+            type: String,
+            required: true,
+        },
+        reactions: [reactionSchema]
+    }
+);
