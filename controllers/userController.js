8afa3377454bf1a8426742b088e7ea57ae8a261e
Justin Liu <justinwl2097@gmail.com>
2022-11-04 20:28:14 -0500

    added update and delete routes for users

--- controllers/userController.js
+++ controllers/userController.js
@@ -35,5 +35,27 @@ module.exports = {
         User.create(req.body)
             .then((user) => res.json(user))
             .catch((err) => res.status(500).json(err));
+    },
+    updateUser(req, res) {
+        User.findOneAndUpdate(
+            {_id: req.params.userId },
+            {$set: req.body },
+            {runValidators: true, new: true}
+        )
+            .then((user) => 
+                !user
+                    ? res.status(404).json({ message: 'No user with that ID' })
+                    : res.json(user)
+            )
+            .catch((err) => res.status(500).json(err));
+    },
+    deleteUser(req, res) {
+        User.findOneAndRemove({ _id: req.params.userId })
+            .then((user) =>
+                !user
+                    ? res.status(404).json({ message: 'No user with that ID' })
+                    : res.json(user)
+            )
+            .catch((err) => res.status(500).json(err));
     }
 };
