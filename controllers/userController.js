6ade35891583d7c426a2b14e490050c8545e6769
Justin Liu <justinwl2097@gmail.com>
2022-11-05 11:52:02 -0500

    added friend adding and deleting

--- controllers/userController.js
+++ controllers/userController.js
@@ -57,5 +57,31 @@ module.exports = {
                     : res.json(user)
             )
             .catch((err) => res.status(500).json(err));
+    },
+    addFriend(req, res) {
+        User.findOneAndUpdate(
+            { _id: req.params.userId },
+            { $addToSet: { friends: ObjectId(req.params.friendId) }},
+            { runValidators: true, new: true}
+        )
+        .then((user) =>
+            !user
+                ? res.status(404).json({ message: 'No user with that ID'})
+                : res.json(user)
+        )
+        .catch((err) => res.status(500).json(err));
+    },
+    deleteFriend(req, res) {
+        User.findOneAndUpdate(
+            { _id: req.params.userId },
+            { $pull: { friends: ObjectId(req.params.friendId) }},
+            { runValidators: true, new: true}
+        )
+        .then((user) =>
+            !user
+                ? res.status(404).json({ message: 'No user with that ID' })
+                : res.json(user)
+        )
+        .catch((err) => res.status(500).json(err)) ;
     }
 };
